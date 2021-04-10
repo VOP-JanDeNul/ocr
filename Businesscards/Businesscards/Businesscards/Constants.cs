c49a3dd0d2118e6292ae814d219bf9341c200520
Willem Dendauw <willem.dendauw@ugent.be>
2021-04-10 10:58:49 +0200

    Update Constants.cs

--- Businesscards/Businesscards/Businesscards/Constants.cs
+++ Businesscards/Businesscards/Businesscards/Constants.cs
@@ -3,14 +3,14 @@
     public static class Constants
     {
         // Endpoint URL
-        public static readonly string RestUrl = "https://businesscardendpoint20210409142733.azurewebsites.net/api/cards";
+        public static readonly string RestUrl = "";
 
         // AzureVisionOCR.cs
-        public static readonly string AVSubscriptionKey = "6f34ceacacf24c228450d156a8ac809f";
+        public static readonly string AVSubscriptionKey = "";
         public static readonly string AVEndPoint = "https://bcocr.cognitiveservices.azure.com/";
 
         // AzurFormRecognizer.cs
-        public static readonly string AFRendpoint = " https://frocr.cognitiveservices.azure.com/";
-        public static readonly string AFRapiKey = "8e110e64c1d84a58912eeb978c04135a";
+        public static readonly string AFRendpoint = "";
+        public static readonly string AFRapiKey = "";
     }
 }
